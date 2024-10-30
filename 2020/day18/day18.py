bda392d7f05d36480c96476950493d4c2b5ef28c
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-10-30 22:40:15 +0000

    day 18 2020 in python

---
+++ 2020/day18/day18.py
@@ -0,0 +1,37 @@
+import fileinput
+from collections import deque
+
+OPS = {
+    "+": lambda a, b: a + b,
+    "*": lambda a, b: a * b
+}
+
+def evaluate(operands, operators, pt2=False):
+    while operands and operators:
+        if pt2 and operators[0] == "*" and "+" in operators:
+            operands.rotate(-1)
+            operators.rotate(-1)
+            continue
+        op = operators.popleft()
+        a, b = operands.popleft(), operands.popleft()
+        operands.appendleft(OPS[op](a, b))
+    return operands[0]
+
+def solve(expr, pt2=False):
+    operands, operators = deque([]), deque([])
+    while expr:
+        token = expr.popleft()
+        if token == ")": 
+            break
+        elif token == "(":
+            operands.append(solve(expr, pt2))
+        elif token in ["+", "*"]:
+            operators.append(token)
+        else:
+            operands.append(int(token))
+    return evaluate(operands, operators, pt2)
+
+data = [line.strip() for line in fileinput.input()]
+for part in [False, True]:
+    exprs = [deque(list(line.replace(" ", ""))) for line in data]
+    print(f"Part {part + 1}: {sum([solve(expr, part) for expr in exprs])}")
