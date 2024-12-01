6e4f81717862058290ec1a0104094a08edc73d68
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-12-01 12:47:17 +0000

    cleanup files

--- utils/runner.js
+++
@@ -1,8 +0,0 @@
-async function runner(part, fn, ...args) {
-  const t1 = Date.now();
-  const ans = await fn(...args);
-  const t2 = Date.now();
-  console.log(`Part ${part}: ${ans} --- ${(t2 - t1) / 1000}ms`);
-}
-
-module.exports = runner;
