8581bd14441749480578ed66e0e36ca12396743e
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-12-02 11:05:03 +0000

    2024 day2

---
+++ 2024/day2/day2.py
@@ -0,0 +1,18 @@
+import fileinput
+
+def solve(row):
+    pairs = list(zip(row, row[1:]))
+    asc = pairs[0] < pairs[1]
+    for a, b in pairs:
+        if not (0 < abs(a-b) <= 3):
+            return False
+        if a > b and asc or b > a and not asc:
+            return False
+    return True
+
+def pt2(row):
+    return any(solve(row[:i] + row[i+1:]) for i in range(len(row)))
+
+data = [[int(n) for n in line.split()] for line in fileinput.input()]
+print(len(list(filter(solve, data))))
+print(len(list(filter(pt2, data))))
