e73456abf000e1400db94a785b393a391fcf1c2a
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-12-03 11:37:29 +0000

    2024 day3

---
+++ 2024/day3/day3.py
@@ -0,0 +1,21 @@
+import re, fileinput
+
+def solve(data, pt2=False):
+    total = 0
+    can_mul = True
+    for line in data:
+        for i in range(len(line)):
+            if pt2 and line[i:].startswith("don\'t()"):
+                can_mul = False
+            elif pt2 and line[i:].startswith("do()"):
+                can_mul = True
+            else:
+                r = re.match("^mul\((\d+),(\d+)\)", line[i:])
+                if r and can_mul:
+                    a, b = (int(x) for x in r.groups())
+                    total += a * b
+    return total
+
+data = [line for line in fileinput.input()]
+print(solve(data))
+print(solve(data, True))
