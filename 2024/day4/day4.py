d766104467648b974af5e42bf09ae0a018076dbe
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-12-04 14:23:20 +0000

    better day4

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -1,40 +1,25 @@
 import fileinput
+from collections import defaultdict
+
+data = [line for line in fileinput.input()]
+R, C = len(data), len(data[0])
+D = defaultdict(str) | {(r, c): data[r][c] for r in range(R) for c in range(C)}
 
 
-def check_ws(coords, word, data):
-    if all(0 <= dr < len(data) and 0 <= dc < len(data[0]) for dr, dc in coords):
-        found = "".join(data[dr][dc] for dr, dc in coords)
-        return found in [word, word[::-1]]
-    return False
+def search(pos, _dir, word, D=D):
+    (r, c), (dr, dc) = pos, _dir
+    letters = [D[(r+dr*i, c+dc*i)] for i in range(len(word))]
+    return "".join(letters) in [word, word[::-1]]
 
 
-def solve1(data):
-    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
-            (0, 1), (1, -1), (1, 0), (1, 1)]
-    xs = [
-        [(r + dr * i, c + dc * i) for i in range(4)]
-        for r in range(len(data))
-        for c in range(len(data[0]))
-        for dr, dc in dirs
-        if data[r][c] == "X"
-    ]
+xs = [(r, c) for r in range(R) for c in range(C) if data[r][c] == "X"]
+ys = [(r, c) for r in range(R) for c in range(C) if data[r][c] == "A"]
 
-    return sum(check_ws(coords, "XMAS", data) for coords in xs)
+dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
+        (0, 1), (1, -1), (1, 0), (1, 1)]
 
+pt1 = sum(search(x, d, "XMAS") for x in xs for d in dirs)
+pt2 = sum(all(search((r-dr, c-dc), (dr, dc), "MAS")
+              for dr, dc in [(1, 1), (1, -1)]) for r, c in ys)
 
-def solve2(data):
-    xs = [
-        [((r-1, c-1), (r, c), (r+1, c+1)),
-         ((r-1, c+1), (r, c), (r+1, c-1))]
-        for r in range(len(data))
-        for c in range(len(data[0]))
-        if data[r][c] == "A"
-    ]
-
-    return sum(
-        all(check_ws(c, "MAS", data) for c in coords) for coords in xs
-    )
-
-
-data = [row for row in fileinput.input()]
-print(solve1(data), solve2(data))
+print(pt1, pt2)
