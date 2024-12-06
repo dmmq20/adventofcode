4d0ce5b8b3004910cac26694cf06606339cc3bac
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-12-06 15:53:14 +0000

    optimize pt2

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -1,6 +1,17 @@
 import fileinput
 
 
+data = [list(row) for row in fileinput.input()]
+R, C = len(data), len(data[0])
+start = [(r, c) for r in range(R) for c in range(C) if data[r][c] == "^"][0]
+dirs = {
+    (-1, 0): (0, 1),
+    (0, 1): (1, 0),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0)
+}
+
+
 def solve(pos, data, pt2=False):
 
     d = (-1, 0)
@@ -17,27 +28,14 @@ def solve(pos, data, pt2=False):
             else:
                 pos = (r+dr, c+dc)
         else:
-            return False if pt2 else len({(r, c) for (r, c), _ in visited})
+            return False if pt2 else {(r, c) for (r, c), _ in visited}
 
 
-data = [list(row) for row in fileinput.input()]
-R, C = len(data), len(data[0])
-start = [(r, c) for r in range(R) for c in range(C) if data[r][c] == "^"][0]
-dirs = {
-    (-1, 0): (0, 1),
-    (0, 1): (1, 0),
-    (1, 0): (0, -1),
-    (0, -1): (-1, 0)
-}
+path = solve(start, data)
+pt2 = 0
+for r, c in path:
+    data[r][c] = "#"
+    pt2 += int(solve(start, data, True))
+    data[r][c] = "."
 
-ans = 0
-for r in range(R):
-    for c in range(C):
-        if (r, c) != start and data[r][c] != "#":
-            data[r][c] = "#"
-            ans += int(solve(start, data, True))
-            data[r][c] = "."
-
-
-print(solve(start, data))
-print(ans)
+print(len(path), pt2)
