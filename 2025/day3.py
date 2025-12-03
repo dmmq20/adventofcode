d13742fe96e71a53e4b292ee3246f14bec287b54
Darren <152389701+dmmq20@users.noreply.github.com>
2025-12-03 16:56:09 +0000

    Update day3.py minor changes

--- 2025/day3.py
+++ 2025/day3.py
@@ -2,38 +2,38 @@ IN = '''987654321111111
 811111111111119
 234234234234278
 818181911112111'''
+
 IN = [list(line.strip()) for line in IN.splitlines()]
 
-def solve(i, length, max_len):
-    if length == max_len:
-        return ""
-
-    if i == len(bank):
-        return None
-
-    if (i, length) in DP:
-        return DP[(i, length)]
-
-    skip = solve(i+1, length, max_len)
-    take = solve(i+1, length+1, max_len)
-    if take is not None:
-        take = bank[i] + take
-
-    if skip is None:
-        best = take
-    elif take is None:
-        best = skip
-    else:
-        best = max(skip, take)
-
-    DP[(i, length)] = best
-    return best
-
-pt1 = pt2 = 0
-for bank in IN:
+def solve(bank, max_len):
     DP = {}
-    pt1 += int(solve(0, 0, 2))
-    DP = {}
-    pt2 += int(solve(0, 0, 12))
+    def _solve(i, length, max_len):
+        if length == max_len:
+            return ""
+    
+        if i == len(bank):
+            return None
+    
+        if (i, length) in DP:
+            return DP[(i, length)]
+    
+        skip = _solve(i+1, length, max_len)
+        take = _solve(i+1, length+1, max_len)
+        if take is not None:
+            take = bank[i] + take
+    
+        if skip is None:
+            best = take
+        elif take is None:
+            best = skip
+        else:
+            best = max(skip, take)
+    
+        DP[(i, length)] = best
+        return best
+    return _solve(0, 0, max_len)
 
+
+pt1 = sum(int(solve(bank, 2)) for bank in IN)
+pt2 = sum(int(solve(bank, 12)) for bank in IN)
 print(pt1, pt2)
