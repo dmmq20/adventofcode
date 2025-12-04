0a4b7d8f1cbd5a015e543e67382b876656eb6f47
Darren <152389701+dmmq20@users.noreply.github.com>
2025-12-04 14:07:41 +0000

    Update day4.py

--- 2025/day4.py
+++ 2025/day4.py
@@ -10,28 +10,23 @@ IN = '''..@@.@@@@.
 @.@.@@@.@.'''
 
 IN = [list(line.strip()) for line in IN.splitlines()]
+R, C = len(IN), len(IN[0])
+adj = [(i, j) for i in [-1, 0, 1] for j in [-1, 0, 1] if (i, j) != (0, 0)]
 
 def solve(pt2=False):
-    adj = [(i, j) for i in [-1, 0, 1] for j in [-1, 0, 1] if (i, j) != (0, 0)]
-    ans, changed = 0, True
-    while changed:
-        changed = False
-        for r in range(len(IN)):
-            for c in range(len(IN[0])):
-                if IN[r][c] != "@":
-                    continue
-                cnt = 0
-                for dr, dc in adj:
-                    if dr+r < 0 or dc+c < 0:
-                        continue
-                    if dr+r >= len(IN) or dc+c >= len(IN[0]):
-                        continue
-                    cnt += IN[r+dr][c+dc] == "@"
-                ans += cnt < 4
-                if pt2 and cnt < 4:
-                    IN[r][c] = "."
-                    changed = True and pt2
-    
+    pts = {(r, c) for r in range(R) for c in range(C) if IN[r][c] == "@"}
+    ans, removed = 0, set()
+    while True:
+        pts ^= removed
+        removed.clear()
+        for r, c in pts:
+            nbrs = sum((r+dr, c+dc) in pts for dr, dc in adj)
+            if nbrs < 4:
+                removed.add((r, c))
+        ans += len(removed)
+        if pt2 and len(removed) > 0:
+            continue
+        break
     return ans
     
 print(solve(), solve(True))
