61560b87fd5b0b0f046d3324a9314ba507dd316b
Darren <152389701+dmmq20@users.noreply.github.com>
2025-12-05 16:15:18 +0000

    aoc 2025 day3 update with cleaner solution

--- 2025/day3.py
+++ 2025/day3.py
@@ -3,37 +3,22 @@ IN = '''987654321111111
 234234234234278
 818181911112111'''
 
-IN = [list(line.strip()) for line in IN.splitlines()]
-
 def solve(bank, max_len):
     DP = {}
-    def _solve(i, length, max_len):
-        if length == max_len:
+    def _solve(idx=0, curr_len=0, max_len=0):
+        if curr_len == max_len or idx >= len(bank):
             return ""
-    
-        if i == len(bank):
-            return None
-    
-        if (i, length) in DP:
-            return DP[(i, length)]
-    
-        skip = _solve(i+1, length, max_len)
-        take = _solve(i+1, length+1, max_len)
-        if take is not None:
-            take = bank[i] + take
-    
-        if skip is None:
-            best = take
-        elif take is None:
-            best = skip
-        else:
-            best = max(skip, take)
-    
-        DP[(i, length)] = best
-        return best
+        take = bank[idx] + _solve(idx+1, curr_len+1, max_len)
+        skip = _solve(idx+1, curr_len, max_len)
+        if skip:
+            DP[(idx, curr_len)] = str(max(int(take), int(skip)))
+            return DP[(idx, curr_len)]
+        DP[(idx, curr_len)] = take
+        return DP[(idx, curr_len)]  
+        
     return _solve(0, 0, max_len)
 
-
-pt1 = sum(int(solve(bank, 2)) for bank in IN)
-pt2 = sum(int(solve(bank, 12)) for bank in IN)
+banks = IN.splitlines()
+pt1 = sum(int(solve(bank, 2)) for bank in banks)
+pt2 = sum(int(solve(bank, 12)) for bank in banks)
 print(pt1, pt2)
