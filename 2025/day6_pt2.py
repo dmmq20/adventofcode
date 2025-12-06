6d9f9c5252d5cc13265ee258f704f420b98bf27d
Darren <152389701+dmmq20@users.noreply.github.com>
2025-12-06 15:17:23 +0000

    Update day6_pt2.py

--- 2025/day6_pt2.py
+++ 2025/day6_pt2.py
@@ -3,7 +3,7 @@ IN = '''123 328  51 64
   6 98  215 314
 *   +   *   +  '''
 
-IN = [line.replace(" ", ".") for line in IN.splitlines()]
+IN = [line for line in IN.splitlines()]
 cols = list(zip(*IN))
 
 OPS = {"+": lambda x, y: x + y, "*": lambda x, y: x * y}
@@ -11,15 +11,12 @@ ptr, pt2, total, op = 0, 0, None, None
 while ptr < len(cols):
     k = cols[ptr]
     ptr += 1
-    if all(ch == "." for ch in k):
+    if all(ch == " " for ch in k):
         pt2 += total
         total = op = None
         continue
     if not op:
         op = k[-1]
-    x = int("".join(ch for ch in k if ch not in [".", "+", "*"]))
-    if not total:
-        total = x
-    else:
-        total = OPS[op](total, x)
+    x = int("".join(ch for ch in k if ch not in ["+", "*"]))
+    total = x if not total else OPS[op](total, x)
 print(pt2 + total)
