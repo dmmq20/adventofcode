3d83bb2e7b438df0f31b1262a8abd5ec368a5dc3
Darren <152389701+dmmq20@users.noreply.github.com>
2025-12-07 11:20:23 +0000

    aoc 2025 day7.py

---
+++ 2025/day7.py
@@ -0,0 +1,50 @@
+from functools import cache
+
+IN = '''.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............'''
+
+IN = [list(line) for line in IN.splitlines()]
+S = [(r, c) for r in range(len(IN)) for c in range(len(IN[0])) if IN[r][c] == "S"][0]
+
+def dfs(start):
+    dfs, seen, splits = [start], set(), 0
+    while dfs:
+        r, c = dfs.pop()
+        if (r, c) in seen or not (0<=r<len(IN) and 0<=c<len(IN)):
+            continue
+        seen.add((r, c))
+        if IN[r][c] == "^":
+            splits += 1
+            dfs.append((r+1, c-1))
+            dfs.append((r+1, c+1))
+        else:
+            dfs.append((r+1, c))
+    return splits
+
+@cache
+def solve(row, col, dir):    
+    if row == len(IN) and 0<=col<len(IN):
+        return 1
+    if not (0<=row<len(IN) and 0<=col<len(IN)):
+        return 0
+    if IN[row][col] == "^":
+        return solve(row+1, col-1, "L") + solve(row+1, col+1, "R")
+    else:
+        return solve(row+1, col, dir)
+
+pt1, pt2 = dfs(S), solve(S[0], S[1], "D")
+print(pt1, pt2)
